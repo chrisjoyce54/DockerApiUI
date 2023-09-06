7aa6275de48a2dd5a88b093d4e8089fef3cc242e
chris <chris@joycenet.us>
2023-09-06 14:55:26 -0500

    Add project files.

---
+++ WebApiForDocker/Controllers/DemoController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiForDocker.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class DemoController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Welcome to the Docker World!";
+        }
+    }
+}
